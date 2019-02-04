76b0dcfc77b1f46e263d50831671b00b43dac678
gkarumba <gachegua@gmail.com>
2019-02-04 18:09:02 +0300

    [finishes #163698845] Adds the .travis.yml file

---
+++ app/tests/v1/test_jobs_endpoints.py
@@ -0,0 +1,54 @@
+import unittest
+import json
+
+from app.api.jobs.v1.models import JobsModel
+from app.tests.v1.test_base import BaseTest
+
+class TestEndpoints(BaseTest):
+    """
+    Class for testing the jobs endpoints
+    """
+    def test_post(self):
+        """ 
+        Method for testing the post job endpoint
+        """
+        response =  self.client.post('api/v1/jobs',\
+                                      data=json.dumps(self.post_data),\
+                                      content_type='application/json')
+        result = json.loads(response.data)
+        # import pdb; pdb.set_trace()
+        self.assertTrue(response.status_code, 201)
+        self.assertIn(result['data'][0]['message'],'job succesfully added')
+
+    def test_get(self):
+        """
+        Method for testing the get job endpoint
+        """
+        self.client.post('api/v1/jobs',\
+                          data=json.dumps(self.post_data2),\
+                          content_type='application/json')
+        response = self.client.get('api/v1/jobs',\
+                         data=json.dumps(self.post_data2),\
+                        content_type='application/json')
+        result = json.loads(response.data)
+        # import pdb; pdb.set_trace()
+        self.assertTrue(response.status_code, 200)
+        self.assertIn(result['data'][0]['message'], 'jobs available')
+
+    def test_get_one(self):
+        """
+        Method for testing the get job by category endpoint
+        """
+        self.client.post('api/v1/jobs',\
+                          data=json.dumps(self.post_data2),\
+                          content_type='application/json')
+        response = self.client.get('api/v1/jobs/1',\
+                         data=json.dumps(self.post_data2),\
+                        content_type='application/json')
+        result = json.loads(response.data)
+        # import pdb; pdb.set_trace()
+        self.assertTrue(response.status_code, 200)
+        self.assertIn(result['data'][0]['message'], 'jobs available')
+
+if __name__ == '__main__':
+    unittest.main()
